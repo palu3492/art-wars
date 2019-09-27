1e1bdeb6d95e73438a11216e2031cd922fdfbeb9
Alex Palumbo <palu3492@stthomas.edu>
2019-09-26 19:43:50 -0500

    Testing

---
+++ js/testPastebinAPI.js
@@ -0,0 +1,16 @@
+
+function testPastebin() {
+    var xhr = new XMLHttpRequest();
+    let pastebinApiUrl = 'https://pastebin.com/api/api_post.php';
+    xhr.open("POST", pastebinApiUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    let requestBody = {
+        api_dev_key: '26338589f548f136db10773ec3eecbed',
+        api_option: 'paste',
+        api_paste_code: 'Artwars', // Content of paste
+        api_paste_name: 'Artwars test', // Title of paste
+        api_paste_private: '1',
+        api_user_key: '83f22f57b6e93b11a6964506361da438', // Login
+    };
+    xhr.send(JSON.stringify(requestBody));
+}
