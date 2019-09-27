95a2cfd731086d978bb2185cf963a3be40e3f0e1
Alex Palumbo <palu3492@stthomas.edu>
2019-09-26 20:11:48 -0500

    Update testPastebinAPI.js

--- js/testPastebinAPI.js
+++ js/testPastebinAPI.js
@@ -14,4 +14,25 @@ function testPastebin() {
         api_user_key: '83f22f57b6e93b11a6964506361da438', // Login
     };
     xhr.send(JSON.stringify(requestBody));
+    console.log(xhr);
+    xhr.onload = function() {
+        if (xhr.status != 200) { // analyze HTTP status of the response
+            alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+        } else { // show the result
+            alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+        }
+    };
+
+    xhr.onprogress = function(event) {
+        if (event.lengthComputable) {
+            alert(`Received ${event.loaded} of ${event.total} bytes`);
+        } else {
+            alert(`Received ${event.loaded} bytes`); // no Content-Length
+        }
+
+    };
+
+    xhr.onerror = function() {
+        alert("Request failed");
+    };
 }
