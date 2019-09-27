9d55258b625c584aca70623ca8d7fc8fe080cfad
Alex Palumbo <palu3492@stthomas.edu>
2019-09-26 19:47:02 -0500

    Update testPastebinAPI.js

--- js/testPastebinAPI.js
+++ js/testPastebinAPI.js
@@ -4,7 +4,7 @@ function testPastebin() {
     let pastebinApiUrl = 'https://pastebin.com/api/api_post.php';
     xhr.open("POST", pastebinApiUrl, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://palu3492.github.io/Art-Wars/');
     let requestBody = {
         api_dev_key: '26338589f548f136db10773ec3eecbed',
         api_option: 'paste',
